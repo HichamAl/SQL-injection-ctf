36bfccfa07d4ec3d79218f8764644258a22d2c86
HichamAl <104017860+HichamAl@users.noreply.github.com>
2023-04-01 01:02:28 +0200

    Update settings.py

--- sql/sql/settings.py
+++ sql/sql/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_ez#&aeayb4l@m+846wj*(#y3bdib&6*-d+jci_r_zzzb!!_iq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
